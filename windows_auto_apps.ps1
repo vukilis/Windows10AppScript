1c1207944aca4da2981d12326f3317db027504aa
vukilis <vuklekic22@gmail.com>
2021-12-14 13:37:25 +0100

    first commit

--- windows_auto_apps.ps1
+++ windows_auto_apps.ps1
@@ -1,16 +1,16 @@
 # Check if winget is installed
 if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
-    'Winget Already Installed'
+    Write-Host 'Winget Already Installed' -ForegroundColor Green
 }  
 else{
     # Installing winget from the Microsoft Store
-	Write-Host "Winget not found, installing it now."
+	Write-Host "Winget not found, installing it now." -ForegroundColor DarkRed
     $ResultText.text = "`r`n" +"`r`n" + "Installing Winget... Please Wait"
 	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
 	$nid = (Get-Process AppInstaller).Id
 	Wait-Process -Id $nid
-	Write-Host `nWinget Installed
-    $ResultText.text = "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task"
+	Write-Host `n"Winget Installed" -ForegroundColor Green
+    $ResultText.text = Write-Host "`r`n" +"`r`n" + "Winget Installed - Ready for Next Task" -ForegroundColor Cyan
 }
 # Write-Host ""
 #get file from web and parse json
@@ -20,19 +20,34 @@ $url = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/vukilis/Windows
 $packages = ConvertFrom-Json $url.content
 
 $number = ($packages).Length
-Write-Host `n"Number of packages: $number"
-# Write-Host ""
+Write-Host `n"Number of packages: $number" -ForegroundColor DarkMagenta
 
+# List all packages
 Write-Host `n"Packages:" -ForegroundColor Magenta
 Write-Host ""
 foreach ($name in $packages)
 {
     try {
-		# winget install -e $name.package | Out-Host
         Write-Host $name.package -ForegroundColor Yellow
 	}
 	catch {
 		Write-Output `n"$($name.package) - $($_.Exception.Message)"
 	}
 }
-
+# Y or N to install packages
+$answer = $(Write-Host "Do you want to proceed instalation? [Yy]/[Nn]: " -NoNewLine -ForegroundColor White) + $(Read-Host) 
+if ("Y" -eq $answer.ToLower()){
+	foreach ($name in $packages)
+	{
+		try {
+			winget install -e $name.package | Out-Host
+			# Write-Host $name.package -ForegroundColor Yellow
+		}
+		catch {
+			Write-Output `n"$($name.package) - $($_.Exception.Message)"
+		}
+	}
+}
+else{
+	Write-Host "Goodbye" -ForegroundColor Red
+}
